3aabf99ed0a43b3ff5e9d52181fd6039e9d43dcc
Anjali Ramlolarakh Pandey <68697438+anjali0503@users.noreply.github.com>
2021-05-11 21:52:18 +0530

    Create ex4.py

---
+++ ex4.py
@@ -0,0 +1,12 @@
+# Exercise 4: Given a string and an integer number n, remove characters from a string starting from zero up to n and return a new string
+def given_str_and_number(string, num):
+    print(char[num:])
+char = "anjali"
+print("after Removing num number of  char and stings becomes :")
+given_str_and_number(char, 3)
+
+
+#output:
+
+after Removing num number of  char and stings becomes :
+ali
